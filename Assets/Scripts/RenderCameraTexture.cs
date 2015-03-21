dafb646c50e1278d86d6fae586a9cdf778cf6f66
Jakub <kubatrt@gmail.com>
2015-03-21 19:46:19 +0100

    First draft

---
+++ Assets/Scripts/RenderCameraTexture.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderCameraTexture : MonoBehaviour {
+
+	public static Texture2D 	renderedTexture;
+	public Material		targetMaterial;
+
+	public void Awake()
+	{
+		renderedTexture = new Texture2D (512, 512);
+		targetMaterial.mainTexture = renderedTexture;
+	}
+
+	public void OnPostRender()
+	{
+		renderedTexture.ReadPixels(new Rect(0, 0, 512, 512), 0, 0);	// Screen.width, Screen.height
+		renderedTexture.Apply ();
+	}
+}
